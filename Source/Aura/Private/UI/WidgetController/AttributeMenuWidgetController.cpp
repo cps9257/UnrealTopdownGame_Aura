ac465b9e0bbe5799799ee54a2180d8c65be99073
rjchang <rjchang@rayark.com>
2024-01-27 18:30:42 +0800

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,13 @@
+// Copyright Rong Jie
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
