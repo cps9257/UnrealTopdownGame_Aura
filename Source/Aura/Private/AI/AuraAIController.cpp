7558b29d4e3d68ffe4966d1fa0184b9c13a8685b
rjchang <rjchang@rayark.com>
2024-03-16 23:45:43 +0800

    AI Perception sight with enemy

--- Source/Aura/Private/AI/AuraAIController.cpp
+++ Source/Aura/Private/AI/AuraAIController.cpp
@@ -24,25 +24,11 @@ ETeamAttitude::Type AAuraAIController::GetTeamAttitudeTowards(const AActor& Othe
 {
 	if (const APawn* OtherPawn = Cast<APawn>(&Other)) {
      
-		// DEFAULT BEHAVIOR---------------------------------------------------
 		if (const IGenericTeamAgentInterface* TeamAgent = Cast<IGenericTeamAgentInterface>(OtherPawn->GetController()))
 		{
 			return Super::GetTeamAttitudeTowards(*OtherPawn->GetController());
 		}
-    
-		// //OR CUSTOM BEHAVIOUR--------------------------------------------------
-		// if (const IGenericTeamAgentInterface* TeamAgent = Cast<IGenericTeamAgentInterface>(OtherPawn->GetController()))
-		// {
-		// 	//Create an alliance with Team with ID 10 and set all the other teams as Hostiles:
-		// 	FGenericTeamId OtherTeamID = TeamAgent->GetGenericTeamId();
-		// 	if (OtherTeamID == FGenericTeamId(NEUTRAL_TEAM_ID)) {
-		// 		return ETeamAttitude::Neutral;
-		// 	}
-		// 	else {
-		// 		return ETeamAttitude::Hostile;
-		// 	}
-		// }
 	}
-
+	
 	return ETeamAttitude::Neutral;
 }
