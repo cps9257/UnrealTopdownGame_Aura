ecf52aa8ee429444b03b520ba9a0e5007b35439b
rjchang <rjchang@rayark.com>
2024-04-07 15:44:55 +0800

    Equipped Row Button

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -118,9 +118,17 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FName("InputTag.3"),
 		FString("Input Tag for 3 key")
 	);
-	GameplayTags.InputTag_RMB = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("InputTag.4"),
 		FString("Input Tag for 4 key")
+	);
+	GameplayTags.InputTag_Passive_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Passive.1"),
+		FString("Input Tag for Passive Ability 1")
+	);
+	GameplayTags.InputTag_Passive_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Passive.2"),
+		FString("Input Tag for Passive Ability 2")
 	);
 
 	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
